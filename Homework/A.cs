8f4386247a5689eda6a8b5e2e87931bc6b8cbe51
Eruslanovs <some@mail.ru>
2021-07-27 00:13:21 +0300

    Вариант выполнения задания Skillbox 11.2

---
+++ Homework/A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    interface I1
+    {
+        public void M();
+    }
+
+    interface I2
+    {
+        public void M();
+    }
+
+    public class A : I1, I2
+    {
+        //public void M() { Console.WriteLine("A.M()"); } 
+        void I1.M() { Console.WriteLine("A.I1.M()"); }
+        void I2.M() { Console.WriteLine("A.I2.M()"); }
+    }
+
+    public class B:A, I1,I2
+    {
+        void I1.M() { Console.WriteLine("B.I1.M()"); }
+        void I2.M() { Console.WriteLine("B.I2.M()"); }
+    }
+
+}
+
---
+++ Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(new A() as I1).M();");
+            (new A() as I1).M();
+            Console.WriteLine("(new A() as I2).M();");
+            (new A() as I2).M();
+            Console.WriteLine("(new B() as I1).M();");
+            (new B() as I1).M();
+            Console.WriteLine("(new B() as I2).M();");
+            (new B() as I2).M();
+            Console.WriteLine("(new B() as A as I1).M();");
+            (new B() as A as I1).M();
+            Console.WriteLine("(new B() as A as I2).M();");
+            (new B() as A as I2).M();
+        }
+    }
+}
